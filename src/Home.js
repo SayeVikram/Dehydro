2f13ce9af7c91e64cf84518e22357f5e8c2f3701
Saye Vikram Karthikeyan <sayevikram@Sayes-MacBook-Pro.local>
2023-06-14 22:27:42 +0530

    made new css changes

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import background from './download.png';
+import './Home.css';
 
 function Home(){
-    const styles = {
-        position: 'absolute',
-        textAlign: 'center',
-        width: '100%',
-        left: "0px"
-    };
+    
 
     const styles_1 = {
         position: 'absolute',
@@ -15,6 +11,14 @@ function Home(){
         width: '100%',
         left: "0px",
         top: "300px"
+    };
+
+    const styles_2 = {
+        position: 'absolute',
+        textAlign: 'center',
+        width: '100%',
+        left: "0px",
+        top: "350px"
     };
 
     const back = {
@@ -25,14 +29,17 @@ function Home(){
     };
 
     return (
-    <body style={back}>
-            <center>
-                
-                <h1 style={styles}> Water is one of the most essential ingredients </h1>
-                <h1 style={styles_1}> BRUHHH </h1>
-            </center>
-            
-    </body>
+    
+
+    <body> 
+    <section class="bubble">
+        <center>
+        <h1 style={styles_1}>HEADER FOR THE ABOUT</h1>
+        <p style={styles_2}>EXPLAINATION</p>
+        </center>
+    </section>
+    </body>   
+ 
     );
 }
 
